a622b26ca5d4fcabe610c78c15609f02f8156550
Lyan <42992@etu.he2b.be>
2019-05-08 12:45:01 +0200

    modification de la structure des tables sql

--- mailman.sql
+++ mailman.sql
@@ -3,18 +3,12 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 02 Mai 2019 à 10:45
+-- Généré le :  Mer 08 Mai 2019 à 12:34
 -- Version du serveur :  10.3.13-MariaDB-1-log
 -- Version de PHP :  7.3.3-1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `alternc`
@@ -26,63 +20,36 @@ SET time_zone = "+00:00";
 -- Structure de la table `mailman`
 --
 
-CREATE TABLE `mailman` (
-  `id` int(10) UNSIGNED NOT NULL,
+
+
+CREATE TABLE IF NOT EXISTS `mailman` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `uid` int(10) UNSIGNED NOT NULL DEFAULT 0,
   `list` varchar(128) NOT NULL DEFAULT '',
   `domain` varchar(255) NOT NULL DEFAULT '',
   `name` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(64) NOT NULL DEFAULT '', -- password is not used in mailman3 because list don't have password anymore
   `owner` varchar(255) NOT NULL DEFAULT '',
   `url` varchar(255) NOT NULL DEFAULT '',
   `mailman_action` enum('OK','CREATE','DELETE','PASSWORD','GETURL','SETURL','DELETING','REGENERATE','REGENERATE-2') NOT NULL DEFAULT 'OK',
-  `mailman_result` varchar(255) NOT NULL DEFAULT ''
+  `mailman_result` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `mailman_action` (`mailman_action`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Mailman mailing lists';
 
--- --------------------------------------------------------
 
 --
 -- Structure de la table `mailman_account`
 --
 
-CREATE TABLE `mailman_account` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `mailman_account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(10) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL,
+  `mailman_action` enum('OK','CREATE','DELETE') NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uid` (`uid`,`username`,`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `mailman`
---
-ALTER TABLE `mailman`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `mailman_action` (`mailman_action`);
-
---
--- Index pour la table `mailman_account`
---
-ALTER TABLE `mailman_account`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `uid` (`uid`,`username`,`email`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `mailman`
---
-ALTER TABLE `mailman`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
---
--- AUTO_INCREMENT pour la table `mailman_account`
---
-ALTER TABLE `mailman_account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
